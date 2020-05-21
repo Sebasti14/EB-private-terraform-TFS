2ae53f3b94d7bf0bba475625b58ad98a15586928
Sebasti14 <51194064+Sebasti14@users.noreply.github.com>
2020-05-21 19:30:20 +0530

    Network configuration

---
+++ network_config.tf
@@ -0,0 +1,74 @@
+## Get the VPC ID ##
+
+data "aws_vpc" "eb-vpc" {
+  id = var.vpc_id
+# filter {
+#    name = var.vpc_name
+#   values = var.vpc_name
+}
+
+
+## Create security groups for EB VPC endpoints ##
+
+#resource "aws_security_group" "allow-vpc" {
+#  name        = "allow_vpc_eb"
+#  description = "Allow inbound traffic from VPC"
+#  vpc_id      = data.aws_vpc.eb-vpc.id
+#
+# ingress {
+#    description = "TLS from VPC"
+#    from_port   = 443
+#    to_port     = 443
+#    protocol    = "tcp"
+#    cidr_blocks = data.aws_vpc.eb-vpc.cidr_block
+#  }
+
+#  ingress {
+#    description = "HTTP from VPC"
+#    from_port   = 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = data.aws_vpc.eb-vpc.cidr_block
+#  }
+
+
+#  egress {
+#    from_port   = 0
+#   to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+
+#  tags = {
+#    Name = "allow_vpc_eb"
+#  }
+#}
+
+
+## Create Interface VPC endpoints ##
+
+#resource "aws_vpc_endpoint" "eb-int-ep" {
+#  vpc_id            = data.aws_vpc.eb-vpc.id
+#  service_name      = "com.amazonaws.${var.aws_region}.${var.interface_endpoints}"
+#  vpc_endpoint_type = "Interface"
+
+#  security_group_ids = [
+#    aws_security_group.allow-vpc.id,
+#  ]
+#  subnet_ids = ["${var.subnet_ids}"]
+#
+#  private_dns_enabled = true
+#}
+
+## Create Gateway VPC endpoint - S3 ##
+
+#resource "aws_vpc_endpoint" "s3" {
+#  vpc_id       = data.aws_vpc.eb-vpc.id
+#  service_name = "com.amazonaws.${var.aws_region}.s3"
+#}
+
+## Create route table for S3 ##
+
+#resource "aws_route_table" "s3_route" {
+#  vpc_id = data.aws_vpc.eb-vpc.id
+#}
