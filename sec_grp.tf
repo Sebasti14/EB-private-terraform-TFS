407727d3b012242a93cb5bb0256607b2ab4c451b
Sebasti14 <51194064+Sebasti14@users.noreply.github.com>
2020-05-21 19:35:49 +0530

    Security group for LB

---
+++ sec_grp.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "eb-lb-sg" {
+  vpc_id = aws_vpc.main-vpc.id
+  name = "eb-lb-sg"
+  description = "AWSEB-LBsecuritygroup"
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "AWSEB-LBsg"
+  }
+}
+
+#resource "aws_security_group" "eb-vpc-sg" {
+#  vpc_id = aws_vpc.main-vpc.id
+#  name = "eb-vpc-sg"
+#  description = "AWSEB-VPCsecuritygroup"
+#  egress {
+#    from_port = 0
+#    to_port = 0
+#    protocol = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  egress {
+#    from_port = 123
+#    to_port = 123
+#    protocol = "udp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  ingress {
+#    from_port = 22
+#    to_port = 22
+#    protocol = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  ingress {
+#    from_port = 80
+#    to_port = 80
+#    protocol = "tcp"
+#    security_groups = [aws_security_group.eb-lb-sg.id]
+#  }
+#  ingress {
+#    from_port = 123
+#    to_port = 123
+#    protocol = "udp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  tags = {
+#    Name = "AWSEB-VPCsg"
+#  }
+#}
