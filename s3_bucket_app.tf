56e7f421d041ee2142ec23a4efa48cf113022589
Sebasti14 <51194064+Sebasti14@users.noreply.github.com>
2020-05-25 19:04:09 +0530

    Update s3_bucket_app.tf

--- s3_bucket_app.tf
+++ s3_bucket_app.tf
@@ -12,15 +12,24 @@ resource "aws_s3_bucket" "eb-bucket" {
   }
 }
 
-data " aws_s3_bucket" "eb-bucket" {
+data "aws_s3_bucket" "eb-bucket" {
   count = var.create_s3_bucket == true ? 0 : 1
+  bucket = var.s3_bucket_name
+  }
 
 resource "aws_s3_bucket_object" "eb-bucket-object" {
   count = var.create_s3_bucket == true ? 1: 0
-  bucket = "EB-${var.aws_region}-environment"
+  bucket = aws_s3_bucket.eb-bucket.id
   key    = "eb-${var.app_name}-appcode/sample_tomcat.war"
   source = "./sample_tomcat.war"
   depends_on = [
     aws_s3_bucket.eb-bucket,
     ]
 }
+
+data "aws_s3_bucket_object" "eb-bucket-object" {
+  count = var.create_s3_bucket == true ? 0 : 1
+  bucket = aws_s3_bucket.eb-bucket.id
+  key = var.s3_object_path
+  }
+  
