88b265f4b3dbc76e729806360dfecaba3369f73e
Sebasti14 <51194064+Sebasti14@users.noreply.github.com>
2020-05-25 18:44:51 +0530

    Update s3_bucket_app.tf

--- s3_bucket_app.tf
+++ s3_bucket_app.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "eb-bucket" {
+  count = var.create_s3_bucket == true ? 1 : 0
   bucket = "EB-${var.aws_region}-environment"
 #  bucket_prefix = "elasticbeanstalk-${var.aws_region}-${var.app_name}"
   region = var.aws_region
@@ -11,7 +12,11 @@ resource "aws_s3_bucket" "eb-bucket" {
   }
 }
 
+data " aws_s3_bucket" "eb-bucket" {
+  count = var.create_s3_bucket == true ? 0 : 1
+
 resource "aws_s3_bucket_object" "eb-bucket-object" {
+  count = var.create_s3_bucket == true ? 1: 0
   bucket = "EB-${var.aws_region}-environment"
   key    = "eb-${var.app_name}-appcode/sample_tomcat.war"
   source = "./sample_tomcat.war"
