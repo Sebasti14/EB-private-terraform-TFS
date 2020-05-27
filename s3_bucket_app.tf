5f169c10ee8af3157ac2855246ac141c2f4fc632
Sebasti14 <51194064+Sebasti14@users.noreply.github.com>
2020-05-27 14:47:35 +0530

    Update s3_bucket_app.tf

--- s3_bucket_app.tf
+++ s3_bucket_app.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "eb-bucket" {
   count = var.create_s3_bucket == true ? 1 : 0
-  bucket = "EB-${var.aws_region}-environment"
+  bucket = "eb-${var.aws_region}-environment"
 #  bucket_prefix = "elasticbeanstalk-${var.aws_region}-${var.app_name}"
   region = var.aws_region
   acl    = "private"
@@ -11,6 +11,7 @@ resource "aws_s3_bucket" "eb-bucket" {
     enabled = true
   }
 }
+
 
 data "aws_s3_bucket" "eb-bucket" {
   count = var.create_s3_bucket == true ? 0 : 1
